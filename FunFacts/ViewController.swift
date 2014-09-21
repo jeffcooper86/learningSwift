cfc28e84291751535a2e16315c73b64a2e90ac90
Jeff Cooper <jeffcooperjc@gmail.com>
2014-09-21 11:41:34 -0400

    New single view xcode project

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Jeffrey Cooper on 9/21/14.
+//  Copyright (c) 2014 Jeff Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
